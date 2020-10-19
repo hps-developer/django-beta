f2a973e634fe983d8f9a03ad920b7d3a340be3e7
Sukhbat <ssuheesh@gmail.com>
2020-10-19 16:16:51 +0800

    search by closing date

--- umoney/views.py
+++ umoney/views.py
@@ -554,7 +554,7 @@ class TransactionAggregationInquiryReqList(
     pagination_class = CustomPageNumberPagination
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['terminal_id', 'id']
-    search_fields = ['transmission_datetime']
+    search_fields = ['closing_date']
     ordering_fields = ['created']
 
     def get(self, request, *args, **kwargs):
@@ -644,7 +644,7 @@ class TransactionAggregationInquiryRespList(
     pagination_class = CustomPageNumberPagination
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['terminal_id', 'id', 'response_code']
-    search_fields = ['transmission_datetime']
+    search_fields = ['closing_date']
     ordering_fields = ['created']
 
     def get(self, request, *args, **kwargs):
