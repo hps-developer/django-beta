d4a6710530f172cfbb0f45cf43a825a73a563a3e
chagimgl <chagimgl@gmail.com>
2021-01-15 03:21:40 +0800

    rotation added

--- identificationRecognition/face_detection.py
+++ identificationRecognition/face_detection.py
@@ -18,27 +18,47 @@ import os
 def countFace(selfie):
 
     faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    image = imread(selfie, pilmode="RGB")
-    image = image.copy()
+    
+    image_original = imread(selfie, pilmode="RGB")
+    image_rotate_90_counterclockwise = cv2.rotate(image_original, cv2.ROTATE_90_COUNTERCLOCKWISE)
+    image_rotate_90_clockwise = cv2.rotate(image_original, cv2.ROTATE_90_CLOCKWISE)
+    image_rotate_180 = cv2.rotate(image_original, cv2.ROTATE_180)
 
+    image_original = image_original.copy()
+    image_rotate_90_counterclockwise = image_rotate_90_counterclockwise.copy()
+    image_rotate_90_clockwise = image_rotate_90_clockwise.copy()
+    image_rotate_180 = image_rotate_180.copy()
+
+    image_array = []
+    image_array.append(image_original)
+    image_array.append(image_rotate_90_counterclockwise)
+    image_array.append(image_rotate_90_clockwise)
+    image_array.append(image_rotate_180)
+
+    
     # ratio = image.shape[0] / 100.0
     # orig = image.copy()
     # image = imutils.resize(image, height = 700)
 
+    face_count = 0
 
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.2,
-        minNeighbors=5,
-        minSize=(10, 10),
-        flags = cv2.CASCADE_SCALE_IMAGE
-    )
+    for image in image_array:
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor=1.2,
+            minNeighbors=5,
+            minSize=(10, 10),
+            flags = cv2.CASCADE_SCALE_IMAGE
+        )
+        face_found = len(faces)
 
-    print ("Found {0} faces.".format(len(faces)))
-    return len(faces)
+        if face_found > 0:
+            break
 
-    # uncomment below to draw rectangle around faces found
+    return face_found
+
+    # use below to draw rectangle around faces found
     # for (x, y, w, h) in faces:
     #     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
     # cv2.imshow('image', image)
