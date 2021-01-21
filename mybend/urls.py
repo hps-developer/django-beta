2cddbd727e0d5b014a53162d41ec7a292e3492a4
Chagi <chagimgl@gmail.com>
2021-01-21 16:33:36 +0800

    cleaned umoney

--- mybend/urls.py
+++ mybend/urls.py
@@ -24,7 +24,6 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-umoney/', include('umoney.urls')),
     path('api-quickstart/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include('identificationRecognition.urls')),
--- umoney/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UmoneyConfig(AppConfig):
-    name = 'umoney'
