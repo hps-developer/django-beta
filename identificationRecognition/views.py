51af57aadba1ab640c68468862b351dac90c4c64
gantugs <gantugs01300130@gmail.com>
2022-03-31 15:50:57 +0800

    delete model

--- identificationRecognition/views.py
+++ identificationRecognition/views.py
@@ -31,6 +31,8 @@ def cropImageLocal(img):
     model = torch.hub.load('ultralytics/yolov5', 'custom', path = 'id_rec/yolov5/runs/train/exp9/weights/best.pt', force_reload=False)
     get_image = idcropper.Cropper(img,model)
     linku =  get_image.crop()
+    del get_image
+    del model
     return linku
 
 
