1db35ed878602069ea29bb37a308fc5a50d25d90
itgl <itgel6708@gmail.com>
2022-04-12 12:28:35 +0800

    removed cropImageLocak

--- identificationRecognition/views.py
+++ identificationRecognition/views.py
@@ -44,14 +44,6 @@ def GetdataListLocal(listu):
         
     return res
         
-def cropImageLocal(img):
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path = 'id_rec/yolov5/runs/train/exp9/weights/best.pt', force_reload=False)
-    get_image = idcropper.Cropper(img,model)
-    linku =  get_image.crop()
-    return linku
-
-
-
 def CropListImageLocal(listu):
     model = torch.hub.load('ultralytics/yolov5', 'custom', path = 'id_rec/yolov5/runs/train/exp9/weights/best.pt', force_reload=False)
     res = []
